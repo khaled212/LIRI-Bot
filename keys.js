6f16b7da8d55f40ab482b2d9c3de6272f4de0ba1
khaled hammouda <khaledehammouda@icloud.com>
2020-02-01 14:35:46 -0500

    fixed it

--- keys.js
+++ keys.js
@@ -1,15 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: "5934943fd6b94d04add210669b53f676",
+  secret: "c49b28e78be84663bb4167372aa60d9a"
 };
-
-// Client ID 5934943fd6b94d04add210669b53f676
-// Client Secret c49b28e78be84663bb4167372aa60d9a
-
-
-// module.exports = {
-//     exports: spotify = keys
-    
-//       };
